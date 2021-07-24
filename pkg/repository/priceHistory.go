302061e605878fc98aec7c2d30fc5596e1cf8065
mkd <monkeydioude@gmail.com>
2021-07-24 19:53:25 +0200

    changed context timeout for coins history

--- pkg/repository/priceHistory.go
+++ pkg/repository/priceHistory.go
@@ -12,7 +12,7 @@ type PriceHistory struct {
 }
 
 func NewPriceHistory() *PriceHistory {
-	ctx, cancel := context.WithTimeout(context.Background(), 2*time.Second)
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	return &PriceHistory{
 		BaseRepo: BaseRepo{
 			context:    ctx,
